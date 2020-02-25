0e70e28f813e04b885dac2375466433b1f3c515e
Akshita Agrawal <aagrawal4@hawk.iit.edu>
2020-02-25 15:35:57 -0600

    added confirm function

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ jQuery(function($) {
   $('html').addClass('hasjs');
 });
 document.getElementById("submit").addEventListener("click", confirm);
+
+function confirm(event) {
+  for(var i = 0; i < document.getElementsByTagName("input").length; i++) {
+    if(document.getElementsByTagName("input").item(i).value === "" || (document.getElementsByTagName("input").item(i).type === "radio") && (document.getElementsByTagName("input").item(i).checked === false)) {
+      return false;
+    } else {
+      name = document.getElementById("name").value;
+      alert("Thank You " + name + "," + " for signing up!\n");
+      return true;
+    }
+  }
+}
